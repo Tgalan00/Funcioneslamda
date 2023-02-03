7a3041be227410b30ab5cd934e72df0130faf4cb
GALÁNGUZMÁNTONI <toni.galan@enti.cat>
2023-02-03 14:28:42 +0100

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,76 @@
+import kotlin.math.pow
+
+fun dialectalEOf(sentence: List<Char>): List<Char> {
+
+    val vocalsList = listOf('a', 'e', 'i', 'o', 'u')
+    val sentenceWithE = sentence.map {
+
+        if (it in vocalsList) {
+
+            'e'
+
+        } else {
+
+            it
+        }
+    }
+    return sentenceWithE
+}
+
+fun <E> similar(l1: List<E>, l2: List<E>): Boolean {
+    return l1.all { l2.contains(it) } && l2.all { l1.contains(it) }
+}
+
+fun anonymousMathOperation(a: Int, b: Int, operation: (Int, Int) -> Double): Double{
+    return operation(a,b)
+}
+
+enum class Dialect(val vocal: Char) {
+    A('a'), E('e'), I('i'), O('o'), U('u')
+}
+
+fun dialectalOf(sentence: String, dialect: Dialect): String {
+
+    val vocalsList = listOf('a', 'e', 'i', 'o', 'u')
+    val sentenceWithE = sentence.map {
+
+        if (it in vocalsList) {
+            dialect.vocal
+        } else {
+            it
+        }
+    }
+    return  String(sentenceWithE.toCharArray())
+}
+
+fun productOfPairs(list: List<Int>): Double{
+    return list.filter { it %2 == 0 }.reduce { acc, it ->
+        acc*it
+    }.toDouble()
+}
+
+fun powers(list: List<Int?>): List<Int>{
+    return list.filterNotNull().mapIndexed { index, it ->
+        (it.toDouble().pow(index)).toInt()
+    }
+}
+
+fun <E> equals2(l1: List<E>, l2: List<E>): Boolean {
+    if (l1.size != l2.size){
+        return false
+    }
+    l1.zip(l2) { e1, e2 ->
+        if (e1 != e2) {
+            return false
+        }
+    }
+    return true
+}
+
+
+fun main() {
+
+
+
+}
+
